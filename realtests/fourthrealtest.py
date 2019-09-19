a538dcf97ee30b29f7f95c74fdd7d72cb0631a5d
Aravind Balasubramanian <bmn.arvind@gmail.com>
2019-09-19 16:01:25 -0400

    updated challenge

---
+++ realtests/fourthrealtest.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/planets")
+def planets():
+    force_dict = {} 
+    for i in range(1,11):
+        force_json = requests.get(f'https://swapi.co/api/planets/{i}/?format=json').json()
+        force_dict[force_json.get("name")] = force_json.get("climate") 
+    return jsonify(force_dict)
+
+if __name__ == "__main__":
+	app.run(port=5006)
